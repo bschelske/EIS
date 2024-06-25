466d218ec824843f14d070805a1ad082925f6870
bschelske <103594627+bschelske@users.noreply.github.com>
2024-06-25 17:24:32 -0500

    plot both nyquist and bode

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def compare_nyquist(df_list: list[pd.DataFrame]) -> Union[Any, Any]:
 
 
 def bode_plot(df: pd.DataFrame) -> Union[Any, Any]:
-    """Makes single nyquist plot from list of pandas dataframes"""
+    """Makes single bode plot from pandas dataframe"""
 
     fig, axes = plt.subplots(2, 1, figsize=(7, 6.66))
     x = np.log10(df['Freq/Hz'])
@@ -105,7 +105,7 @@ def bode_plot(df: pd.DataFrame) -> Union[Any, Any]:
 
 
 def bode_plot_overlay(df: pd.DataFrame) -> Union[Any, Any]:
-    """Makes single nyquist plot from list of pandas dataframes"""
+    """Makes bode plot with overlay from pandas dataframe"""
 
     fig, ax1 = plt.subplots(figsize=(3.33, 3.33))
     ax2 = ax1.twinx()
@@ -126,11 +126,87 @@ def bode_plot_overlay(df: pd.DataFrame) -> Union[Any, Any]:
 
     return fig, ax1
 
+
+def nyquist_and_bode(df: pd.DataFrame) -> Union[Any, Any]:
+    """Makes nyquist and bode plot from pandas dataframe"""
+
+    fig, ax1 = plt.subplots(1,2, figsize=(7, 3.5))
+    plt.gcf().subplots_adjust(bottom=0.15)
+
+    ax1[0].plot(df["Z'/ohm"], df['Z"/ohm'] * -1)
+
+    # Each axis range should be equal
+    new_limit = min_x_max_y(ax1[0].get_xlim(), ax1[0].get_ylim())
+    ax1[0].set_xlim(new_limit)
+    ax1[0].set_ylim(new_limit)
+
+    ax1[0].set_xlabel("Z'/ohm")
+    ax1[0].set_ylabel('-Z"/ohm')
+    ax1[0].spines[['right', 'top']].set_visible(False)
+
+    ax2 = ax1[1].twinx()
+    x = np.log10(df['Freq/Hz'])
+
+    # Log(|Z|) vs Log(Frequency)
+    ax1[1].plot(x, np.log10(df["Z/ohm"].abs()), color='black', label='|Z|')
+    # ax1.set_ylim(0, 7)
+    ax1[1].set_xlabel("log(Freq/Hz)")
+    ax1[1].set_ylabel("log(Z/ohm)")
+
+    # -Phase vs Log(Frequency)
+    ax2.plot(x, df['Phase/deg'] * -1, color='red', label='phase')
+    # ax2.set_ylim(ax2.get_ylim()[0] - 40, ax2.get_ylim()[1] + 40)
+    ax2.set_xlabel("log(Freq/Hz)")
+    ax2.set_ylabel('-Phase/deg')
+    fig.legend(loc="lower left", bbox_to_anchor=(0, 0), bbox_transform=ax1[1].transAxes, frameon=False)
+
+    return fig, ax1
+
+
+def compare_both(df_list: list[pd.DataFrame]) -> Union[Any, Any]:
+    """Makes nyquist and bode plot from pandas dataframe"""
+
+    fig, ax1 = plt.subplots(1,2, figsize=(7, 3.5))
+    plt.gcf().subplots_adjust(bottom=0.15)
+    ax2 = ax1[1].twinx()
+    for df in df_list:
+        ax1[0].plot(df["Z'/ohm"], df['Z"/ohm'] * -1)
+        x = np.log10(df['Freq/Hz'])
+
+        # Log(|Z|) vs Log(Frequency)
+        ax1[1].plot(x, np.log10(df["Z/ohm"].abs()), color='black', label='|Z|')
+
+        # -Phase vs Log(Frequency)
+        ax2.plot(x, df['Phase/deg'] * -1, color='red', label='phase')
+
+    # Each axis range should be equal
+    new_limit = min_x_max_y(ax1[0].get_xlim(), ax1[0].get_ylim())
+    ax1[0].set_xlim(new_limit)
+    ax1[0].set_ylim(new_limit)
+    # ax1[1].set_ylim(0, 7)
+
+    ax1[0].set_xlabel("Z'/ohm")
+    ax1[0].set_ylabel('-Z"/ohm')
+    ax1[0].spines[['right', 'top']].set_visible(False)
+
+    ax1[1].set_xlabel("log(Freq/Hz)")
+    ax1[1].set_ylabel("log(Z/ohm)")
+    # ax2.set_ylim(ax2.get_ylim()[0] - 40, ax2.get_ylim()[1] + 40)
+    ax2.set_xlabel("log(Freq/Hz)")
+    ax2.set_ylabel('-Phase/deg')
+
+    # fig.legend(loc="lower left", bbox_to_anchor=(0, 0), bbox_transform=ax1[1].transAxes, frameon=False)
+
+    return fig, ax1
+
+
 def main():
-    path = "data/11/"
+    path = "data/7/"
     files = get_file_paths(path)
-    file = files[1]
+    file = files[0]
     df, file_information = open_file_as_df(file)
+    df.to_csv("7.csv")
+
 
     # Create a nyquist plot
     nyquist = False
@@ -140,7 +216,7 @@ def main():
         plt.show()
 
     # Create a Bode plot
-    bode = True
+    bode = False
     if bode:
         # bode_fig, bode_ax = bode_plot(df)
         # bode_ax[0].set_title(file)
@@ -148,6 +224,28 @@ def main():
         bode_fig, bode_ax = bode_plot_overlay(df)
         bode_ax.set_title(file)
         plt.show()
+
+    # Create Nyquist and Bode
+    both = False
+    if both:
+        both_fig, both_ax = nyquist_and_bode(df)
+        plt.show()
+
+    both_comparison = False
+    if both_comparison:
+        device = "resistor"
+        path = f"data/{device}/"
+        files = get_file_paths(path)
+        df_list = []
+        print(files)
+
+        for file in files:
+            df, _ = open_file_as_df(file)
+            df_list.append(df)
+
+        fig, ax = compare_both(df_list)
+        plt.show()
+        fig.savefig(f"C:/Users/bensc/Desktop/Local Research/EIS/plots/{device}_bn.png")
 
 
 if __name__ == "__main__":
